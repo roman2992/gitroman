73c38e952cbd324d26bffccd0d9d457659167d01
roman <rorokin.r@gmail.com>
2025-10-20 21:41:51 +0300

    + docker-compose.yml, Dockerfile, запись данных в таблицы pg, ch, mongo

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
-# This is a sample Python script.
+import psycopg2
+from clickhouse_driver import Client
+from pymongo import MongoClient
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# PostgreSQL
+pg_conn = psycopg2.connect(
+    host="localhost",
+    port=5432,
+    user="user",
+    password="password",
+    dbname="example_db"
+)
+pg_cursor = pg_conn.cursor()
+pg_cursor.execute("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name TEXT);")
+pg_cursor.execute("INSERT INTO users(name) VALUES (%s)", ("Alice",))
+pg_cursor.execute("SELECT * FROM users;")
+print("PostgreSQL:", pg_cursor.fetchall())
+pg_conn.commit()
+pg_cursor.close()
+pg_conn.close()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+ch_client = Client(
+    host='localhost',
+    user='user',
+    password='strongpassword',
+    port=9000  # TCP порт ClickHouse
+)
+ch_client.execute("CREATE TABLE IF NOT EXISTS test (id UInt32, name String) ENGINE = MergeTree() ORDER BY id")
+ch_client.execute("INSERT INTO test (id, name) VALUES", [(1, 'Bob')])
+rows = ch_client.execute("SELECT * FROM test")
+print("ClickHouse:", rows)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-    print("Hello, GIT!")
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# MongoDB
+mongo_client = MongoClient("mongodb://localhost:27017/")
+mongo_db = mongo_client["test_db"]
+mongo_collection = mongo_db["users"]
+mongo_collection.insert_one({"name": "Charlie"})
+print("MongoDB:", list(mongo_collection.find({}, {"_id": 0})))
