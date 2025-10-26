5b884763d9928cb95fb9114d4562cdf58c2e665d
roman <rorokin.r@gmail.com>
2025-10-26 19:32:04 +0300

    Mongo: основные операции с NoSQL

--- main.py
+++ main.py
@@ -10,4 +10,14 @@ client = MongoClient("mongodb://localhost:27017/")
 db = client["alcomarket"]
 products = db["products"]
 
+# 2. Очистка коллекции перед загрузкой (для повторного запуска)
+products.drop()
 
+# 3. Загрузка данных из файла products.json
+with open("products.json", "r") as f:
+    data = json.load(f)
+    products.insert_many(data)
+
+print("\n📦 Все товары:")
+for doc in products.find():
+    pprint(doc)
